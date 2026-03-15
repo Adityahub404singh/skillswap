675638640cdf3fac9b52d6a332f1114fce0f8a72
Adityahub404singh <singhaditya4560@gmail.com>
2026-03-15 12:48:09 +0530

    fix: explore matchedMentors query fix

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -22,7 +22,7 @@ export default function Explore() {
 
   const { data: matchedMentors, isLoading: mentorsLoading } = useGetMatchedMentors(
     selectedSkill || "",
-    { ...options, enabled: !!selectedSkill }
+    { ...options, enabled: !!selectedSkill } as any
   );
 
   return (
